ba1c1c57cf05528d17047af72891b664705f4f42
Rohith <rohithiitj@gmail.com>
2018-03-24 18:17:28 -0700

    implements simple one column visualizer

---
+++ src/visualizer.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("../database.csv")
+
+def year_wise(data):
+    result = data.groupby("Year").sum()["Victim Count"]
+    result.plot()
+    plt.show()
+
+def race_wise(data):
+    result = data[["Record ID", "Victim Race"]].groupby("Victim Race").count()
+    result.plot(kind="bar")
+    plt.show()
+
+def column_wise(data, column):
+    result = data[["Record ID", column]].groupby(column).count()
+    print(result)
+    if result.shape[0] < 100:
+        result.plot(kind='bar')
+        plt.show()
+
+
+column_wise(data, "Incident")
+
+
+
+
+
+"""
+Record ID,Agency Code,Agency Name,Agency Type,City,State,Year,Month,Incident,Crime Type,
+Crime Solved,Victim Sex,Victim Age,Victim Race,Victim Ethnicity,Perpetrator Sex,Perpetrator Age,
+Perpetrator Race,Perpetrator Ethnicity,Relationship,Weapon,Victim Count,Perpetrator Count,Record Source
+"""
